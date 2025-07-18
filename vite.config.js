d069c8ad96d5dee64bad768b4d58db763953881e
naoki <n.globe.us@gmail.com>
2025-07-18 18:22:18 +0900

    viteのパスをリポジトリ名を自動で設定するように修正

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/jma-wind-map/' : '/',
+  base: `/${process.env.VITE_BASE_PATH}/` || '/',
   build: {
     sourcemap: true
   }
